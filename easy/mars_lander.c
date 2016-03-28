2d7b40fa3f841141ad69ae3242db0772cb24a61f
Pierre BONDOERFFER <pbondoer@student.42.fr>
2016-03-28 23:22:06 +0200

    Easy: Mars Lander - Level 1

---
+++ easy/mars_lander.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int surfaceN; // the number of points used to draw the surface of Mars.
+	scanf("%d", &surfaceN);
+	for (int i = 0; i < surfaceN; i++) {
+		int landX; // X coordinate of a surface point. (0 to 6999)
+		int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+		scanf("%d%d", &landX, &landY);
+	}
+
+	// game loop
+	while (42) {
+		int X;
+		int Y;
+		int hSpeed; // the horizontal speed (in m/s), can be negative.
+		int vSpeed; // the vertical speed (in m/s), can be negative.
+		int fuel; // the quantity of remaining fuel in liters.
+		int rotate; // the rotation angle in degrees (-90 to 90).
+		int power; // the thrust power (0 to 4).
+		scanf("%d%d%d%d%d%d%d", &X, &Y, &hSpeed, &vSpeed, &fuel, &rotate, &power);
+
+		if (vSpeed > -40)
+			printf("0 0\n");
+		else
+			printf("0 4\n");
+	}
+
+	return 0;
+}
